1572e7fe2261bd9eb3d9062b6fa433369db23a05
max <mcnic@users.noreply.github.com>
2024-06-14 23:05:58 -0700

    feat: add lambdas and gateway

---
+++ construct/task03.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import { Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { LambdaIntegration, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+
+export class Task03 extends Construct {
+  constructor(parent: Stack, name: string) {
+    super(parent, name);
+
+    const getProductsListLambda = new Function(
+      this,
+      'getProductsListLambda',
+      {
+        runtime: Runtime.NODEJS_20_X,
+        code: Code.fromAsset('dist/lambda/getProductsList'),
+        handler: 'getProductsList.handler',
+      }
+    );
+
+    const getProductsByIdLambda = new Function(
+      this,
+      'getProductsByIdLambda',
+      {
+        runtime: Runtime.NODEJS_20_X,
+        code: Code.fromAsset('dist/lambda/getProductsById'),
+        handler: 'getProductsById.handler',
+      }
+    );
+
+    const getAllIntegration = new LambdaIntegration(getProductsListLambda);
+    const getOneIntegration = new LambdaIntegration(getProductsByIdLambda);
+
+    const api = new LambdaRestApi(this, 'AWSShopApi', {
+      handler: getProductsListLambda,
+      proxy: false,
+    });
+
+    const items = api.root.addResource('products');
+    items.addMethod('GET', getAllIntegration);
+
+    const singleItem = items.addResource('{id}');
+    singleItem.addMethod('GET', getOneIntegration);
+  }
+}
--- lib/aws-shop-cdk-stack.ts
+++ lib/aws-shop-cdk-stack.ts
@@ -1,16 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Task03 } from '../construct/task03';
 
 export class AwsShopCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsShopCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Task03(this, "LambdaFunctions");
   }
 }
