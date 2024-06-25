00522c091a23bda1ee6a3a6355190e9ab0e843b2
max <mcnic@users.noreply.github.com>
2024-06-25 15:25:50 -0700

    wip: add Table2

---
+++ product-service/src/constructs/db_new.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {
+  AttributeType,
+  Billing,
+  TableEncryptionV2,
+  TableV2,
+} from 'aws-cdk-lib/aws-dynamodb';
+import { config } from '../config';
+
+export class DymanoDBTables extends Construct {
+  public productsTable: TableV2;
+  public stocksTable: TableV2;
+
+  constructor(parent: Stack, name: string) {
+    super(parent, name);
+
+    this.productsTable = new TableV2(this, 'Products', {
+      tableName: config.PRODUCTS_TABLE_NAME,
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      billing: Billing.onDemand(),
+      encryption: TableEncryptionV2.dynamoOwnedKey(),
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    this.stocksTable = new TableV2(this, 'Stocks', {
+      tableName: config.STOCKS_TABLE_NAME,
+      partitionKey: {
+        name: 'product_id',
+        type: AttributeType.STRING,
+      },
+      billing: Billing.onDemand(),
+      encryption: TableEncryptionV2.dynamoOwnedKey(),
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+  }
+}
