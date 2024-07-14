f44213be7957a46a1dcd64cda43607baa70afc2d
max <mcnic@users.noreply.github.com>
2024-07-14 16:02:53 -0700

    wip: use auth handler for basic authoriation

--- import-service/lib/constructs/api.ts
+++ import-service/lib/constructs/api.ts
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
-import { Stack } from 'aws-cdk-lib';
+import { Duration, Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { IFunction } from 'aws-cdk-lib/aws-lambda';
-import { Cors, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import {
+  Cors,
+  LambdaIntegration,
+  RestApi,
+  TokenAuthorizer,
+} from 'aws-cdk-lib/aws-apigateway';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 type HandlersProps = {
   handler: IFunction;
+  basicAuthorizerHandlder: IFunction;
+  sourceArn: string;
 };
 
 export class ImportsAPI extends Construct {
@@ -23,9 +31,27 @@ export class ImportsAPI extends Construct {
       },
     });
 
+    console.log({ api: api.restApiId });
+
+    const authorizer = new TokenAuthorizer(this, 'CustomBasicAuthAuthorizer', {
+      handler: props.basicAuthorizerHandlder,
+      identitySource: 'method.request.header.Authorization',
+      resultsCacheTtl: Duration.seconds(0),
+    });
+
+    // Permission
+    props.basicAuthorizerHandlder.addPermission('ApiGatewayInvokePermissions', {
+      action: 'lambda:InvokeFunction',
+      principal: new ServicePrincipal('apigateway.amazonaws.com'),
+      // sourceArn: `arn:aws:execute-api:us-east-1:645331675526:${api.restApiId}/authorizers/v8gxd5`,
+      sourceArn: `arn:aws:execute-api:us-east-1:645331675526:i6gsu0m0sg`
+      // sourceArn: api.restApiId,
+    });
+
     // add API method with validation
     const items = api.root.addResource('import');
     items.addMethod('GET', new LambdaIntegration(props.handler), {
+      authorizer,
       requestParameters: {
         'method.request.querystring.name': true,
       },
